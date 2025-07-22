c953b6af0512a7dd2d67bf22179359f637cc81e5
WilliamD99 <dnam310199@gmail.com>
2025-07-21 23:09:49 -0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "@/public/assets/scss/index.css";
 import LayoutClient from "./layoutClient";
 import { Poppins, Nunito } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Luci3",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <LayoutClient>
         {children}
       </LayoutClient>
+      <Analytics />
     </html>
   );
 }
