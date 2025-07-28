6a515ef5cdee3c6c47147cabd5d8af5d7573e65c
WilliamD99 <dnam310199@gmail.com>
2025-07-27 22:16:01 -0700

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,10 +3,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Contact",
   description: "Contact me",
-  openGraph: {
-    title: "Contact",
-    description: "Contact me",
-  },
 }
 
 export default function ContactPage() {
